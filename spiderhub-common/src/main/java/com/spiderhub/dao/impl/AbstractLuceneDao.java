1a54b1fe31d0119edab835933dc2039e9a7cf5fd
chenzmGX <495338332@qq.com>
2014-10-26 17:17:08 +0800

    IbaseIndex
    NRT实现

--- spiderhub-common/src/main/java/com/spiderhub/dao/impl/AbstractLuceneDao.java
+++ spiderhub-common/src/main/java/com/spiderhub/dao/impl/AbstractLuceneDao.java
@@ -1,30 +1,42 @@
-package com.spiderhub.dao.impl;
+package com.spiderhub.commons.dao.impl;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.StringReader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 
 import org.apache.log4j.Logger;
 import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.TokenStream;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.index.DirectoryReader;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.index.TrackingIndexWriter;
 import org.apache.lucene.index.IndexWriterConfig.OpenMode;
 import org.apache.lucene.index.IndexableField;
 import org.apache.lucene.index.Term;
 import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.search.ControlledRealTimeReopenThread;
 import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.search.Query;
+import org.apache.lucene.search.ReferenceManager;
+import org.apache.lucene.search.SearcherFactory;
+import org.apache.lucene.search.SearcherManager;
 import org.apache.lucene.search.TermQuery;
 import org.apache.lucene.search.TopDocs;
+import org.apache.lucene.search.highlight.Highlighter;
+import org.apache.lucene.search.highlight.QueryScorer;
+import org.apache.lucene.search.highlight.SimpleSpanFragmenter;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
+import org.apache.lucene.store.RAMDirectory;
 import org.apache.lucene.util.Version;
 
-import com.spiderhub.dao.IBaseIndex;
+import com.spiderhub.commons.dao.IBaseIndex;
 /**
  * 
  * @author chenzm
@@ -33,14 +45,35 @@ import com.spiderhub.dao.IBaseIndex;
 public abstract class AbstractLuceneDao implements IBaseIndex{
 	/**分词器*/
 	protected Analyzer analyzer;
+	
 	/**索引路径*/
 	protected String indexPath;
+	
 	/**批量提交数*/
 	protected int commitSize = 5000;
 	
+	/**索引目录*/
 	protected Directory directory;
 	
-	protected Version matchVersion = Version.LUCENE_4_9;
+	/**版本*/
+	public static Version matchVersion = Version.LUCENE_4_9;
+	
+	/**查询管理者*/
+	protected SearcherManager searchManager = null;
+	
+	/**索引的版本号*/
+	private long maxGen = -1;
+	
+	/**索引写对象*/
+	IndexWriter writer;
+	
+	/**封装索引写对象,该对象的每一个操作都会返回索引当前的版本号*/
+	private TrackingIndexWriter genWriter;
+	
+	/**NRT NRTManager被该类所替换*/
+	protected ControlledRealTimeReopenThread<IndexSearcher> realTimeThread = null;
+	
+//	private final ThreadLocal<Long> lastGens = new ThreadLocal<Long>();
 	
 	protected static Logger logger = Logger.getLogger(LuceneDaoImpl.class);
 	
@@ -48,6 +81,17 @@ public abstract class AbstractLuceneDao implements IBaseIndex{
 		try {
 			this.analyzer = analyzer;
 			this.directory = FSDirectory.open(new File(indexPath));
+			this.writer = getIndexWriter(matchVersion, this.analyzer, this.directory);
+			this.searchManager = new SearcherManager(writer, false , new SearcherFactory());
+			Random random = new Random();
+			final double minReopenSec = 0.01 + 0.05 * random.nextDouble();
+			final double maxReopenSec = minReopenSec * (1.0 + 10 * random.nextDouble());
+			this.genWriter = new TrackingIndexWriter(writer);
+			this.realTimeThread = new ControlledRealTimeReopenThread<IndexSearcher>(
+					this.genWriter, this.searchManager, maxReopenSec, minReopenSec);
+			this.realTimeThread.setName("NRT Reopen Thread");
+			this.realTimeThread.setPriority(Math.min(Thread.currentThread().getPriority()+2, Thread.MAX_PRIORITY));
+			this.realTimeThread.start();
 		} catch (IOException e) {
 			e.printStackTrace();
 			logger.error(e);
@@ -62,124 +106,90 @@ public abstract class AbstractLuceneDao implements IBaseIndex{
 	 */
 	protected abstract Document createDocument(Map doc);
 	
-	/**
-	 * 获取IndexWrite实例
-	 * 
-	 * @param analyzer
-	 * @param indexPath
-	 * @return
-	 * @throws IOException
-	 */
+	
+	@Override
 	public IndexWriter getIndexWriter() throws IOException {
-		IndexWriterConfig iwConfig = new IndexWriterConfig(this.matchVersion,
-				this.analyzer);
-		iwConfig.setOpenMode(OpenMode.CREATE_OR_APPEND);
-		return new IndexWriter(this.directory, iwConfig);
+		return writer;
 	}
 	
+	private IndexWriter getIndexWriter(Version matchVersion, Analyzer analyzer, 
+			Directory directory) throws IOException {
+		IndexWriterConfig iwConfig = new IndexWriterConfig(matchVersion, 
+				analyzer);
+		iwConfig.setOpenMode(OpenMode.CREATE_OR_APPEND);
+		return new IndexWriter(directory, iwConfig);
+	}
+	
+	@Override
 	public void add(Map doc) {
-		IndexWriter writer = null;
 		try {
-			writer = getIndexWriter();
-			writer.addDocument(createDocument(doc));
-			writer.commit();
+			long gen = genWriter.addDocument(createDocument(doc));
+			addMaxGen(gen);
 		} catch (Exception e) {
 			e.printStackTrace();
 			logger.error(e);
 			throw new IllegalStateException(e);
-		} finally {
-			close(writer);
-		}
+		} 
 	}
 	
+	@Override
 	public void batchAdd(List<Map> docs) {
-		IndexWriter writer = null;
 		try {
-			writer = getIndexWriter();  
-			int index = 0;
+			List<Document> d = new ArrayList<Document>(docs.size());
 		    for (Map doc : docs) {  
-		    	writer.addDocument(createDocument(doc));
-		    	if(++index % commitSize == 0) {
-		    		writer.commit();
-		    	}
+		    	d.add(createDocument(doc));
 		    }  
-		    if(index % commitSize != 0)
-		    	writer.commit();
+		    long gen = genWriter.addDocuments(d);
+		    addMaxGen(gen);
 		}catch (Exception e) {
 			e.printStackTrace();
 			logger.error(e);
 			throw new IllegalStateException(e);
-		} finally {
-			close(writer);
-		}
+		} 
 	}
 
-	/***
-	 * 
-	 * 删除方法
-	 * 
-	 * */
+	@Override
 	public void delete(String fieldName, String fieldVaule) {
-		IndexWriter writer = null;
 		try {
-			writer = getIndexWriter();
 			Query q = new TermQuery(new Term(fieldName, fieldVaule));
-			writer.deleteDocuments(q);
-			writer.commit();
+			 long gen = genWriter.deleteDocuments(q);
+			 addMaxGen(gen);
 		} catch (Exception e) {
 			e.printStackTrace();
 			logger.error(e);
 			throw new IllegalStateException(e);
-		} finally {
-			close(writer);
 		}
 	}
 
-	/**
-	 * 批量删除
-	 * 
-	 * @param fieldMap
-	 */
+	@Override
 	public void batchDelete(Map<String, String> fieldMap) {
-		IndexWriter writer = null; 
 		try {
-			writer = getIndexWriter();
 			for (String fieldName : fieldMap.keySet()) {
 				Query q = new TermQuery(new Term(fieldName, fieldMap.get(fieldName)));
-				writer.deleteDocuments(q);
+				long gen = genWriter.deleteDocuments(q);
+				addMaxGen(gen);
 			}
-			writer.commit();
 		}catch(Exception e) {
 			e.printStackTrace();
 			logger.error(e);
 			throw new IllegalStateException(e);
-		}finally {
-			close(writer);
 		}
 	}
 
-	/**
-	 * 
-	 * @param fieldName
-	 * @param fieldVaule
-	 * @param stuff
-	 * @throws Exception
-	 */
+	@Override
 	public void update(String fieldName, String fieldVaule,  Map udoc) {
-		IndexWriter writer = null;
 		try {
-			writer = getIndexWriter();
-			IndexSearcher isearcher = new IndexSearcher(DirectoryReader.open(directory));
+			IndexSearcher search = getFinalSearcher();
 			QueryParser qp = new QueryParser(matchVersion, fieldName, analyzer);
 			org.apache.lucene.search.Query query = qp.parse(fieldVaule);
 			System.out.println(query.toString());
-			TopDocs topDocs = isearcher.search(query, 10);
+			TopDocs topDocs = search.search(query, 10);
 			if(topDocs.scoreDocs.length == 0)
 				throw new IllegalArgumentException("索引中没有匹配的结果");
 			if(topDocs.scoreDocs.length > 1) {
 				throw new IllegalArgumentException("匹配到的结果超过一个");
 			}
-			Document doc = isearcher.doc(topDocs.scoreDocs[0].doc);
+			Document doc = search.doc(topDocs.scoreDocs[0].doc);
 			for(IndexableField field : createDocument(udoc).getFields()) {
 				if(doc.get(field.name()) != null) {
 					doc.removeFields(field.name());//可能是多值域
@@ -188,30 +198,8 @@ public abstract class AbstractLuceneDao implements IBaseIndex{
 					doc.add(field);
 				}
 			}
-			writer.updateDocument(new Term(fieldName, fieldVaule), doc);
-		} catch (Exception e) {
-			e.printStackTrace();
-			logger.info(e);
-			throw new IllegalStateException(e);
-		}finally {
-			close(writer);
-		}
-	}
-	
-	public List<Document> search(String searchField, String searchKeyStr,
-			int topCount) {
-		try {
-			IndexSearcher isearcher = new IndexSearcher(DirectoryReader.open(this.directory));
-			QueryParser qp = new QueryParser(this.matchVersion, searchField, this.analyzer);
-//			qp.setDefaultOperator(QueryParser.AND_OPERATOR);
-			Query query = qp.parse(searchKeyStr);
-			logger.info(query.toString(searchField));
-			TopDocs topDocs = isearcher.search(query, topCount);
-			List<Document> docs = new ArrayList<Document>(
-					topDocs.scoreDocs.length);
-			for (int i = 0; i < topDocs.scoreDocs.length; i++)
-				docs.add(isearcher.doc(topDocs.scoreDocs[i].doc));
-			return docs;
+			long gen = genWriter.updateDocument(new Term(fieldName, fieldVaule), doc);
+			addMaxGen(gen);
 		} catch (Exception e) {
 			e.printStackTrace();
 			logger.info(e);
@@ -219,10 +207,66 @@ public abstract class AbstractLuceneDao implements IBaseIndex{
 		}
 	}
 
-	public void forceMerge(int maxSegments, boolean await)  {
-		IndexWriter writer = null;
+	@Override
+	public List<Document> search(String searchField, String searchKeyStr, int topCount) {
+		IndexSearcher search = null;
 		try {
-			writer = getIndexWriter();
+			search = getFinalSearcher();
+			QueryParser qp = new QueryParser(matchVersion, searchField, this.analyzer);
+			Query query = qp.parse(searchKeyStr);
+			logger.info(query.toString(searchField));
+			TopDocs topDocs = search.search(query, topCount);
+			List<Document> docs = new ArrayList<Document>(topDocs.scoreDocs.length);
+			for (int i = 0; i < topDocs.scoreDocs.length; i++)
+				docs.add(search.doc(topDocs.scoreDocs[i].doc));
+			return docs;
+		} catch (Exception e) {
+			e.printStackTrace();
+			logger.info(e);
+			throw new IllegalStateException(e);
+		}finally {
+			release(search);
+		}
+	}
+
+	@Override
+	public List<String> searchToHighlighter(String fieldName, String fieldValue, int topCount) {
+		IndexSearcher search = null;
+		try {
+			QueryParser parser = new QueryParser(matchVersion, fieldName, analyzer);
+			Query query = parser.parse(fieldValue);
+			search = getFinalSearcher();
+			TopDocs topDocs = search.search(query, null, topCount);// 查找
+			QueryScorer scorer = new QueryScorer(query);
+			Highlighter highlight = new Highlighter(scorer);
+			highlight.setTextFragmenter(new SimpleSpanFragmenter(scorer));
+			Document document;
+			TokenStream tokenStream;
+			List<String> lightStrs = new ArrayList<String>(topDocs.scoreDocs.length);
+			for (int i = 0; i < topDocs.scoreDocs.length; i++) {// 获取查找的文档的属性数据
+				int docID = topDocs.scoreDocs[i].doc;
+				document = search.doc(docID);
+				String str = "";
+				String value = document.get(fieldName);
+				if (value != null) {
+					tokenStream = analyzer.tokenStream(fieldName, new StringReader(value));
+					str = str + highlight.getBestFragment(tokenStream, value);
+				}
+				lightStrs.add(str);
+			}
+			return lightStrs;
+		}catch(Exception e) {
+			e.printStackTrace();
+			logger.info(e);
+			throw new IllegalStateException(e);
+		}finally {
+			release(search);
+		}
+	}
+
+	@Override
+	public void forceMerge(int maxSegments, boolean await)  {
+		try {
 			writer.forceMerge(maxSegments, await);
 		}catch(Exception e) {
 			e.printStackTrace();
@@ -232,7 +276,37 @@ public abstract class AbstractLuceneDao implements IBaseIndex{
 			close(writer);
 		}
 	}
-
+	
+	@Override
+	public void doClose() {
+		this.realTimeThread.close();
+		close(writer);
+	}
+	
+	private void release(IndexSearcher search) {
+		try {
+			searchManager.release(search);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private IndexSearcher getFinalSearcher() throws IOException, InterruptedException {
+		this.realTimeThread.waitForGeneration(maxGen);
+		return this.searchManager.acquire();
+	}
+	
+//	protected void doAfterIndexingThreadDone() {
+//	    Long gen = lastGens.get();
+//	    if (gen != null) {
+//	      addMaxGen(gen);
+//	    }
+//	}
+	 
+	private synchronized void addMaxGen(long gen) {
+	    maxGen = Math.max(gen, maxGen);
+	}
+	
 	protected void close(IndexWriter writer) {
 		try {
 			if (writer != null)
